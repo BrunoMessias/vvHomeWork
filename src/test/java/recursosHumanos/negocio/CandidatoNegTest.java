678ced4f3b4cc20f59e2a78eb222f7435fe3f782
Bruno dos Santos Messias <Bruno dos Santos Messias>
2016-06-22 16:02:26 -0300

    Adicionados testes referentes a data de nascimento

--- src/test/java/recursosHumanos/negocio/CandidatoNegTest.java
+++ src/test/java/recursosHumanos/negocio/CandidatoNegTest.java
@@ -12,9 +12,6 @@ import java.util.Date;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Bruno Messias on 15/06/2016.
- */
 public class CandidatoNegTest {
     @Test
     public void validaNome1() throws Exception {
@@ -26,41 +23,77 @@ public class CandidatoNegTest {
 
     @Test
     public void validaNome2() throws Exception {
-        Cargo cargo = new Cargo(null, 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
-        Candidato candidato = new Candidato("Jean Marcos", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato(null, new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
         CandidatoNeg candidatoNeg = new CandidatoNeg();
         candidatoNeg.validaNome(candidato);
         assertFalse(candidatoNeg.validaNome(candidato));
     }
+
     @Test
     public void validaNome3() throws Exception {
-        Cargo cargo = new Cargo("", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
-        Candidato candidato = new Candidato("Jean Marcos", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
         CandidatoNeg candidatoNeg = new CandidatoNeg();
         candidatoNeg.validaNome(candidato);
         assertFalse(candidatoNeg.validaNome(candidato));
     }
+
     @Test
     public void validaNome4() throws Exception {
-        Cargo cargo = new Cargo("0a51", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
-        Candidato candidato = new Candidato("Jean Marcos", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("0a51", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
         CandidatoNeg candidatoNeg = new CandidatoNeg();
         candidatoNeg.validaNome(candidato);
         assertFalse(candidatoNeg.validaNome(candidato));
     }
+
     @Test
     public void validaNome5() throws Exception {
-        Cargo cargo = new Cargo("José_Pedro", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
-        Candidato candidato = new Candidato("Jean Marcos", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("José_Pedro", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
         CandidatoNeg candidatoNeg = new CandidatoNeg();
         candidatoNeg.validaNome(candidato);
         assertFalse(candidatoNeg.validaNome(candidato));
     }
 
     @Test
-    public void validaDataNascimento() throws Exception {
-
+    public void validaNome6() throws Exception {
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("Pedro", new Date(1,1,1990), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        CandidatoNeg candidatoNeg = new CandidatoNeg();
+        candidatoNeg.validaNome(candidato);
+        assertFalse(candidatoNeg.validaNome(candidato));
     }
+
+    @Test
+    public void validaDataNascimento1() throws Exception {
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("Pedro Paulo", new Date(1,1,1940), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        CandidatoNeg candidatoNeg = new CandidatoNeg();
+        candidatoNeg.validaNome(candidato);
+        assertFalse(candidatoNeg.validaNome(candidato));
+    }
+
+    @Test
+    public void validaDataNascimento2() throws Exception {
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("Pedro Paulo", new Date(1,1,2000), EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        CandidatoNeg candidatoNeg = new CandidatoNeg();
+        candidatoNeg.validaNome(candidato);
+        assertFalse(candidatoNeg.validaNome(candidato));
+    }
+
+    @Test
+    public void validaDataNascimento3() throws Exception {
+        Cargo cargo = new Cargo("Estudante", 900.00, EnumTipoDocumentoHabilitacao.CNH, 35);
+        Candidato candidato = new Candidato("Pedro Paulo", null, EnumSexo.M, EnumTipoNacionalidade.BRASILEIRA, EnumTipoDocumentoPessoal.CPF, "7686313618", "32645185630", "", "7378368");
+        CandidatoNeg candidatoNeg = new CandidatoNeg();
+        candidatoNeg.validaNome(candidato);
+        assertFalse(candidatoNeg.validaNome(candidato));
+    }
+
+    
 
     @Test
     public void validarCPF() throws Exception {
